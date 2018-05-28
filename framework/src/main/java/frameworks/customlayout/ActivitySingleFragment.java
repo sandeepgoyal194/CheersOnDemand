440316adad641fed8b5783a31aac49378c137a49
Madhvi <meetmadhvi@gmail.com>
2018-05-28 20:54:48 +0530

    Project Setup

---
+++ framework/src/main/java/frameworks/customlayout/ActivitySingleFragment.java
@@ -0,0 +1,29 @@
+package frameworks.customlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.universe.villiger.R;
+
+import frameworks.basemvp.AppBaseActivity;
+import frameworks.basemvp.AppBaseFragment;
+import frameworks.basemvp.IPresenter;
+
+public abstract class ActivitySingleFragment<T extends AppBaseFragment> extends AppBaseActivity {
+    String TAG = "SingleFragment";
+    @Override
+    public int getViewToCreate() {
+        return R.layout.activity_single_fragment;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addFrgment(getFragment(),TAG,R.id.container,true);
+    }
+    @Override
+    public IPresenter getPresenter() {
+        return null;
+    }
+    protected abstract T getFragment();
+}
